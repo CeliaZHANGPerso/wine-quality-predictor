66f62a5cc738d4ad4ef8cb6eac4c917ed59c6e0f
Celia ZHANG <celiazhang349@gmail.com>
2025-03-12 17:13:25 +0000

    reset requirements.txt for setup.py, read requirements to install from requirements.txt

--- setup.py
+++ setup.py
@@ -6,17 +6,16 @@ ver_path = convert_path('wqp/__init__.py')
 with open(ver_path) as ver_file:
     exec(ver_file.read(), main_ns)
 
+with open('requirements.txt') as f:
+    required = f.read().splitlines()
+
 setuptools.setup(
     name='wqp',
     version='1.0.0',
     author='my_email@email.com',
     description='Wine quality predictor - a packaged machine learning algorithm to predict wine quality',
     packages=setuptools.find_packages(),
-    install_requires=[
-        "scikit-learn==1.6.1",
-        "pandas==2.2.3",
-        "click==7.0"
-    ],
+    install_requires=required,
     entry_points='''
         [console_scripts]
         wqp=wqp.cli:wqp
